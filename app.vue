90238aa6550627c5b2886151772cba027d47d8f0
autoclickvn <autoclickvn@gmail.com>
2024-10-18 20:55:26 +0700

    Update UI responsive

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full relative">
     <img src="/images/background-top.webp" class="w-full h-[600px] absolute top-0 bg-cover left-0 z-0" />
-    <div class="w-full bg-cover bg-no-repeat px-5 md:px-8 lg:px-96 pt-4 md:pt-8 z-10 relative min-h-[70vh]">
+    <div
+      class="w-full bg-cover bg-no-repeat px-5 md:px-8 lg:px-96 2xl:px-[600px] pt-4 md:pt-8 z-10 relative min-h-[70vh]"
+    >
       <Header />
       <NuxtPage />
     </div>
--- layouts/footer/Footer.vue
+++ layouts/footer/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-full px-5 md:px-8 lg:px-96 relative">
+  <footer class="w-full px-5 md:px-8 lg:px-96 2xl:px-[600px] relative">
     <img src="/images/background-bottom.webp" class="w-full h-[500px] absolute bottom-0 bg-cover left-0 z-0" />
     <div class="w-full z-10 pt-10 pb-5 md:pt-20 md:pb-10 relative">
       <div class="w-full h-[1px] bg-[#f5f5f5]" />
